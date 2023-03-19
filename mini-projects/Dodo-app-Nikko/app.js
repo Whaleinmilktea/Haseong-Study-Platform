83b6c038ed5f8692dadc21ec7fb13130a88d2e57
KangHaSeong <shaawwert6044@gmail.com>
2023-03-19 14:42:52 +0900

    add btn - oop

--- mini-projects/Dodo-app-Nikko/app.js
+++ mini-projects/Dodo-app-Nikko/app.js
@@ -2,17 +2,24 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
-const onLoginBtnClick = () => {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("이름을 입력해주세요!")
-  } else if (username.length > 15) {
-    alert("8자 이내의 이름을 입력해주세요!")
-  }
+const onLoginSubmit = (info) => {
+  info.prevenDefault();
+  console.log(info);
+
+  // const username = loginInput.value;
+  // console.log(username)
 };
 
-loginButton.addEventListener("click", onLoginBtnClick);
 
+loginForm.addEventListener("submit", onLoginSubmit)
 
+// function() ===> JS가 함수를 즉시 실행할.
 
+// const onLoginSubmit = (info) => {
+//   info.prevenDefault();
+//   console.log(info);
+// };
 
+// 자바스크립트는 인수로 받은 info를
+// 다음에 실행할 addEvnetListner가 event를 감지할 때에
+// 실행될 onloginSubmit 함수에 전달받은 arg를 참조값으로 전달한다.
