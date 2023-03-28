a719e8cf05b7e0cddf86b8cad082ff6a034e6255
KangHaSeong <shaawwert6044@gmail.com>
2023-03-28 17:04:15 +0900

    add graphql

---
+++ QraphQL/graphql-practice/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Agorastates from './components/Agorastates';
+import IsLogin from './components/Islogin';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Agorastates />
+      <IsLogin />
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
