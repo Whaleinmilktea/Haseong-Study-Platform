b7a7b9ae8f4e80480b3fe1f88894cb74b5d59274
KangHaSeong <shaawwert6044@gmail.com>
2023-03-31 22:41:59 +0900

    add react-router-dom

--- React/React-Router-DOM/learning-spa/src/App.js
+++ React/React-Router-DOM/learning-spa/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to my website!</p>
     </div>
   );
-}
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>You can contact me at:</p>
+      <ul>
+        <li>Phone: 555-555-5555</li>
+        <li>Email: me@example.com</li>
+      </ul>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" exact element={About />}>
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
