45846ff8b0ddb6c960dbde3d9a7f9c77c66ea52b
KangHaSeong <shaawwert6044@gmail.com>
2023-04-05 23:17:20 +0900

    todo project start

--- mini-projects/my-movie-app/src/App.js
+++ mini-projects/my-movie-app/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, )
+
   return (
     <div className="App">
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
     </div>
   );
-}
+};
 
 export default App;
