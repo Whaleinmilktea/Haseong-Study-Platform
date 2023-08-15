1bbe7d729dd458a052ccfcaf96b740424a3a82ef
KangHaSeong <shaawwert6044@gmail.com>
2023-08-15 16:01:28 +0900

    알람시계

--- algorithm/Bronze3/2884번: 알람 시계/알람 시계.py
+++ algorithm/Bronze3/2884번: 알람 시계/알람 시계.py
@@ -9,3 +9,15 @@
 #    Solved: 2023/08/15 15:31:58 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
+import sys
+input = sys.stdin.readline
+
+h, m = map(int, input().split())
+
+alert = (h*60+m) - 45
+alert_h = alert//60
+alert_m = alert - alert_h*60
+if (alert_h < 0):
+    alert_h = alert_h + 24
+
+print(f"{alert_h} {alert_m}")
