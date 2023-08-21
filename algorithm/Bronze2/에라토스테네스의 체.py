c20c6ce5b85dd7467038169de2d91bf54cc1f5fc
KangHaSeong <shaawwert6044@gmail.com>
2023-08-22 07:22:37 +0900

    벌집 문제 푸는중

--- algorithm/Bronze2/에라토스테네스의 체.py
+++ algorithm/Bronze2/에라토스테네스의 체.py
@@ -1,19 +1,23 @@
 # 에라토스테네스의 체 구현
+#
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
+prime = [i for i in range(1, n+1)] # 2부터 주어진 수까지의 전체배열
+prime[0] = 0
 
-# 2~N까지의 전체 리스트 생성
-index_is_prime = []
+# [2, 3, 4, 5, ... , n]
 
-for i in range(2, N+1):
-    index_is_prime.append(i) # 1~N까지의 전체리스트 생성
+for i in range(0, n-1):
+    if (prime[i] == 0): continue # 이미 지워진 수에 대해서는 처리하지 않음
+    for j in range(i+i, n-1, i):
+        prime[j] = 0
 
-for i in range(N-1):
-    if i % 2 == 0:
-        index_is_prime[i] = None
-        for j in range(2*i, N-1, i):
-            index_is_prime[j] = None
+print(prime)
 
-print(index_is_prime)
+result_prime = [] # 결과를 프린트할 새로운 배열
+
+# for i in (1, n-2):
+#     if prime[i] != 0:
+#         result_prime.append(prime[i])
