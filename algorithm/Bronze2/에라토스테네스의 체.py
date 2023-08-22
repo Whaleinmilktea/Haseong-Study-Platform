4da53e5a392b5a5d1a3f774dfcc6d7f07b6bbf82
KangHaSeong <shaawwert6044@gmail.com>
2023-08-22 09:24:39 +0900

    에라토스테네스의 체 구현

--- algorithm/Bronze2/에라토스테네스의 체.py
+++ algorithm/Bronze2/에라토스테네스의 체.py
@@ -4,20 +4,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-prime = [i for i in range(1, n+1)] # 2부터 주어진 수까지의 전체배열
-prime[0] = 0
+prime = [i for i in range(0, n+1)] # 문법 : 리스트 컴프리핸션 ==> 0부터 주어진 수까지 자연수로 이뤄진 배열
+prime[1] = 0
 
-# [2, 3, 4, 5, ... , n]
+for i in range(0, len(prime)):
+    if (prime[i]==0): # 인덱스가 가리키는 원소가 0일 때는, 아무것도 실행하지 않고 다음 i를 반복
+        continue
+    for j in range(2*i, n+1, i): # j = i+i부터 할당하여 i만큼 증가 ==> 배수반복
+            prime[j] = 0 # 배수 반복된 인덱스의 원소는 0으로 재할당 ==> 이후 i인덱스가 해당 원소를 호출할 때는 continue문으로 무시
 
-for i in range(0, n-1):
-    if (prime[i] == 0): continue # 이미 지워진 수에 대해서는 처리하지 않음
-    for j in range(i+i, n-1, i):
-        prime[j] = 0
-
-print(prime)
-
-result_prime = [] # 결과를 프린트할 새로운 배열
-
-# for i in (1, n-2):
-#     if prime[i] != 0:
-#         result_prime.append(prime[i])
+prime_list = [p for p in prime if p != 0]
+print(prime_list)
