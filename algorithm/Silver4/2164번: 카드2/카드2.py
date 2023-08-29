351bc48d6a24f39c3408a58453d0c08527ecc36d
KangHaSeong <shaawwert6044@gmail.com>
2023-08-29 15:11:11 +0900

    카드2

--- algorithm/Silver4/2164번: 카드2/카드2.py
+++ algorithm/Silver4/2164번: 카드2/카드2.py
@@ -9,3 +9,17 @@
 #    Solved: 2023/08/28 19:09:15 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for i in range(1, n+1):
+    q.append(i) # deque([1,2,3,4,5,6])
+
+while len(q) > 1: # 1이랑 같아질 때까지 실행x, 1보다 클 때"만" 실행
+    q.popleft()
+    pop_value = q.popleft()
+    q.append(pop_value)
+
+print(q.pop())
