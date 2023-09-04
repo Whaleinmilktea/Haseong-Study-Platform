d6fe494a9e0a288f511187ab84fa393a53bed91e
KangHaSeong <shaawwert6044@gmail.com>
2023-09-04 15:36:44 +0900

    서비스 레이아웃 설정

---
+++ Practice/next_practice/inflearn-lesson/components/service_layout.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+interface Props {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const ServiceLayout = function ({ title = 'whaleinmilktea', children }: Props) {
+  return (
+    <>
+    {/* html을 기반으로 한 레이아웃의 문법 => 이 자체를 렌더링하지 않고, 별도로 export  */}
+      <Head>
+        <title>{title}</title>
+      </Head>
+      {children}
+    </>
+  );
+};
--- Practice/next_practice/inflearn-lesson/model/firebase_admin.ts
+++ Practice/next_practice/inflearn-lesson/model/firebase_admin.ts
@@ -1,12 +1,13 @@
-import * as admin from "firebase-admin"
+import * as admin from 'firebase-admin';
 // import ~ as 문법은 모듈 이름 그대로가 아닌 모듈을 다른 이름으로 불러오는 것
 
-interface Config { // ? 타입을 왜 이렇게 정한거지? => 강의에서는 "기본명세라고 되어있습니다"
+interface Config {
+  // ? 타입을 왜 이렇게 정한거지? => 강의에서는 "기본명세라고 되어있습니다"
   credential: {
     privateKey: string;
     clientEmail: string;
     projectId: string;
-  }
+  };
 }
 
 // Singleton-class : 파이어베이스 관련 설정을 로드하고 초기화하는 로직
@@ -23,25 +24,25 @@ export default class FirebaseAdmin {
       // TODO : 환경을 초기화한다
       FirebaseAdmin.instance.bootstrap();
     }
-  return FirebaseAdmin.instance;
+    return FirebaseAdmin.instance;
   }
 
   private bootstrap(): void {
     const haveApp = admin.apps.length !== 0;
     if (haveApp) {
       this.init = true; // 클래스 내부에서 선언된 init이기 때문에, 접근하여 변경할 수 있음
-      return
+      return;
     }
 
     const config: Config = {
       credential: {
-        projectId: process.env.projectId || "",
-        clientEmail: process.env.clientEmail || "",
-        privateKey: (process.env.privateKey || "").replace(/\\n/g, "\n")
-      }
-    }
-    admin.initializeApp({ credential: admin.credential.cert(config.credential)});
-    console.info("bootstrap firebase admin")
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        privateKey: (process.env.privateKey || '').replace(/\\n/g, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin');
   }
 
   /** Firestore를 반환 */
