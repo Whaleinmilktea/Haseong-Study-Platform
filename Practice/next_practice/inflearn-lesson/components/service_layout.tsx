960c4d3e2183a2f6f1ba19284e6023fe8b1e939b
KangHaSeong <shaawwert6044@gmail.com>
2023-09-05 14:23:48 +0900

    너무 많은 버전 불일치로 인해, 문제해결에 시간을 더 쏟게 됨. 이로 인해 클론 종료

--- Practice/next_practice/inflearn-lesson/components/service_layout.tsx
+++ Practice/next_practice/inflearn-lesson/components/service_layout.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const ServiceLayout = function ({ title = 'whaleinmilktea', children }: Props) {
   return (
     <>
-    {/* html을 기반으로 한 레이아웃의 문법 => 이 자체를 렌더링하지 않고, 별도로 export  */}
+    {/* html을 기반으로 한 레이아웃의 문법 => 이 자체를 렌더링하지 않고, 별도로 export */}
       <Head>
         <title>{title}</title>
       </Head>
