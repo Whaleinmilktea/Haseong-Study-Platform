5c7ce54e7a1366b8f8f67ce0bca5232c1c81b2cb
KangHaSeong <shaawwert6044@gmail.com>
2023-09-17 01:05:17 +0900

    레이아웃 폼 설정

--- Practice/firebase-practice/src/components/layout.tsx
+++ Practice/firebase-practice/src/components/layout.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Menu = styled.div``;
+const MenuItems = styled.div``;
 
 const Layout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <Wrapper>
+      <Menu>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+        <MenuItem></MenuItem>
+      </Menu>
+    </Wrapper>
   );
 };
 
