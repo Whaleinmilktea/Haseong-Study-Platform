d2be4a21f61ee5417ff5e2e3b531f23bc1c97eeb
KangHaSeong <shaawwert6044@gmail.com>
2023-09-21 01:20:07 +0900

    트윗, 이미지 브라우저 업로드 구현 && 이후 파이버베이스 연동 구현 예정

--- Practice/firebase-practice/src/components/post-tweet-form.tsx
+++ Practice/firebase-practice/src/components/post-tweet-form.tsx
@@ -1,20 +1,97 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
-const Form = styled.form``;
-const TextArea = styled.textarea``;
-const AttachFileButton = styled.label``;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  border-radius: 20px;
+  padding: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
 const AttachFileInput = styled.input`
   display: none;
 `;
-const SubmitButton = styled.input``;
+const SubmitButton = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
 
 const PostTweetForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const onTweetChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    setIsLoading(true)
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+    setIsLoading(false)
+  };
+
   return (
     <Form>
-      <TextArea placeholder="무슨 일이 일어나고 있나요?" />
-      <AttachFileButton htmlFor="file">Add photo</AttachFileButton>
-      <AttachFileInput id="file" type="file" accept="image/*" />
-      <SubmitButton type="submit"/>
+      <TextArea
+        rows={5}
+        maxLength={200}
+        value={tweet}
+        onChange={onTweetChange}
+        placeholder="무슨 일이 일어나고 있나요?"
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "Photo Added" : "Add photo"}
+      </AttachFileButton>
+      <AttachFileInput
+        id="file"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+      />
+      <SubmitButton
+        type="submit"
+        value={isLoading ? "Posting.." : "Post Tweet"}
+      />
     </Form>
   );
 };
