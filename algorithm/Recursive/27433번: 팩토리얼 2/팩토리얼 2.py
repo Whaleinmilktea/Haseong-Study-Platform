9623f6d28490e2df4ef5f3857286a1abe45bca86
KangHaSeong <shaawwert6044@gmail.com>
2023-09-25 19:43:47 +0900

    팩토리얼

--- algorithm/Recursive/27433번: 팩토리얼 2/팩토리얼 2.py
+++ algorithm/Recursive/27433번: 팩토리얼 2/팩토리얼 2.py
@@ -13,6 +13,9 @@ n = int(input())
 
 def factorial(n):
     if n == 0 or n == 1:
-        print(1)
+        return 1
     else:
-        print(n * factorial(n - 1))
+        return n * factorial(n - 1)
+
+result = factorial(n)
+print(result)
