157b10a4fb1b07441c36fc115dc977386c64f876
KangHaSeong <shaawwert6044@gmail.com>
2023-10-05 18:43:44 +0900

    부녀회장 문제 초기화 - dp 이해필요

--- algorithm/Bronze1/2775번: 부녀회장이 될테야/부녀회장이 될테야.py
+++ algorithm/Bronze1/2775번: 부녀회장이 될테야/부녀회장이 될테야.py
@@ -9,16 +9,3 @@
 #    Solved: 2023/08/28 18:33:11 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
-#
-
-T = int(input())
-dp = [[0]*15 for _ in range(15)]
-dp[0] = [x for x in range(15)]
-
-for i in range(1, 15):
-  for j in range(1, 15):
-    for k in range(i, j+1):
-      dp[i][j] += dp[i-1][j]
-
-for _ in range(t):
-  k, n = int(input().split())
