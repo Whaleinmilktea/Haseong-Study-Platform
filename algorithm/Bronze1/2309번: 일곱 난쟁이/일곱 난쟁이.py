079e5581709599777c97f07a93f26093b4b88d17
KangHaSeong <shaawwert6044@gmail.com>
2023-10-31 00:42:56 +0900

    일곱난쟁이 - 브루트포스

--- algorithm/Bronze1/2309번: 일곱 난쟁이/일곱 난쟁이.py
+++ algorithm/Bronze1/2309번: 일곱 난쟁이/일곱 난쟁이.py
@@ -9,25 +9,22 @@
 #    Solved: 2023/10/11 22:00:29 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
-# 입력받은 후 리스트에 저장
-# 리스트의 총합 sum()
-# 리스트 순회 = 두 수의 합 체크 === sum() - 100
-# 리스트에서 제외한 후 출력
-m_list = []
-a = 0
-b = 0
+import sys
+input = sys.stdin.readline
 
-for _ in range(9):
-  m_list.append(int(input()))
+arr = [int(input()) for _ in range(9)]
+s1 = 0
+s2 = 0
 
-left_age = 100-sum(m_list)
+for i in range(9):
+  for j in range(i+1, 9):
+    if sum(arr) - (arr[i] + arr[j]) == 100:
+      s1 = arr[i]
+      s2 = arr[j]
 
-for i in range(len(m_list)-1):
-  for j in range(len(m_list)-2):
-    if m_list[i] + m_list[j+1] == left_age:
-      a = i, b = j
+arr.remove(s1)
+arr.remove(s2)
+arr.sort()
 
-# m_list.pop(m_list[i])
-# m_list.pop(m_list[j])
-
-print(m_list)
+for i in arr:
+  print(i)
