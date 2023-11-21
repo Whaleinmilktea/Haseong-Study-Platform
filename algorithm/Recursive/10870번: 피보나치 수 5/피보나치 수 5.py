2fc02d72ad8ba902d7f40848b15929a471d2ba8f
KangHaSeong <shaawwert6044@gmail.com>
2023-11-21 19:52:26 +0900

    재귀-피보나치

--- algorithm/Recursive/10870번: 피보나치 수 5/피보나치 수 5.py
+++ algorithm/Recursive/10870번: 피보나치 수 5/피보나치 수 5.py
@@ -9,15 +9,24 @@
 #    Solved: 2023/09/26 21:14:43 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
+import sys
+input = sys.stdin.readline
 
-n = int(input())
+m = {}
 
-def recur_fibonacci(n):
-  if n <= 0:
+def fibo(n):
+# 종료조건
+  if n == 0:
     return 0
   elif n == 1:
     return 1
+  # 만약 memo 안에 값이 있다면 해당 값을 리턴
+  elif fibo(n) in m[n]:
+    return m[n]
   else:
-    return recur_fibonacci(n-1) + recur_fibonacci(n-2)
+    # 없을경우, 새로운 값을 추가
+    m[n] = fibo(n-1) + fibo(n-2)
+    return m[n]
 
-print(recur_fibonacci(n))
+n = int(input())
+print(fibo(n))
