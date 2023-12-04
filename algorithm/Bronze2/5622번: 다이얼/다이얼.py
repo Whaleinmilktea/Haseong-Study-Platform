8c88fac0f6cfa42bbaf05a39013eacff1168ad25
KangHaSeong <shaawwert6044@gmail.com>
2023-12-04 16:51:23 +0900

    다이얼

--- algorithm/Bronze2/5622번: 다이얼/다이얼.py
+++ algorithm/Bronze2/5622번: 다이얼/다이얼.py
@@ -10,10 +10,12 @@
 #                                                                              #
 #  **************************************************************************  #
 d = {"ABC":3,"DEF":4,"GHI":5,"JKL":6,"MNO":7,"PQRS":8,"TUV":9,"WXYZ":10}
-cnt=0
+
 num =input().upper()
+cnt=0
+
 for n in num:
     for j in d.keys():
         if str(n) in j:
-            cnt +=d.get(j)
+            cnt += d.get(j)
 print(cnt)
