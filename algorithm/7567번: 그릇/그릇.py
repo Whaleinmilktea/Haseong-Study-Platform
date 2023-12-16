75d5b9ae207612e07ce7854b9c0b2cd0e451c31d
KangHaSeong <shaawwert6044@gmail.com>
2023-12-16 22:30:59 +0900

    약수들의 합

--- algorithm/7567번: 그릇/그릇.py
+++ algorithm/7567번: 그릇/그릇.py
@@ -13,18 +13,31 @@
 # 그릇 = 반대방향 => 10cm
 # 어떤 문제일까? 단순 += 문제일까?
 
-def bowls_height(b):
-  l = list(b)
-  cur = l[0]
-  height = 10
-  for i in l[1:]:
-      if i == cur:
-        height += 5
-        cur = i
-      else:
-        height += 10
-        cur = i
-  return height
+# def bowls_height(b):
+#   l = list(b)
+#   cur = l[0]
+#   height = 10
+#   for i in l[1:]:
+#       if i == cur:
+#         height += 5
+#         cur = i
+#       else:
+#         height += 10
+#         cur = i
+#   return height
+
+# bowls = input()
+# print(bowls_height(bowls))
+
+#! 최적화 코드
+def bowls_height(bowls):
+    height = 10  # 초기 높이
+    for i in range(1, len(bowls)):
+        if bowls[i] == bowls[i-1]:
+            height += 5
+        else:
+            height += 10
+    return height
 
 bowls = input()
 print(bowls_height(bowls))
--- algorithm/Bronze1/1157번: 단어 공부/단어 공부.py
+++ algorithm/Bronze1/1157번: 단어 공부/단어 공부.py
@@ -9,23 +9,4 @@
 #    Solved: 2023/08/28 11:15:50 by shaawwert6044 ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
-s = input().upper()
-d = {}
-
-for el in list(s):
-  if el not in d:
-    d[el] = 1
-  else:
-    d[el] += 1
-
-mx_cnt = max(d.values())
-mx_keys = []
-
-for key, val in d.items():
-  if val == mx_cnt:
-    mx_keys.append(key)
-
-if len(mx_keys) >= 2:
-  print("?")
-else:
-  print(mx_keys[0])
+s = input()
