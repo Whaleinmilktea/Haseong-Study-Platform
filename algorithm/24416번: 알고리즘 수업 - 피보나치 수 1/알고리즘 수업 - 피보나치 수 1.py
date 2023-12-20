183582e54c1cef667ae0258907475b3c3911a015
KangHaSeong <shaawwert6044@gmail.com>
2023-12-20 19:38:25 +0900

    알고리즘 수업 - 피보나치 수 1

--- algorithm/24416번: 알고리즘 수업 - 피보나치 수 1/알고리즘 수업 - 피보나치 수 1.py
+++ algorithm/24416번: 알고리즘 수업 - 피보나치 수 1/알고리즘 수업 - 피보나치 수 1.py
@@ -10,4 +10,16 @@
 #                                                                              #
 #  **************************************************************************  #
 # 피보나치 수를 활용한 dp와 재귀의 비교
+f = [None]*50
+f[1] = f[2] = 1
 
+n = int(input())
+
+def fibonacci(n):
+    for i in range(3, n+1):
+        f[i] = f[i-1]+f[i-2]
+    return f[n]
+
+count1 = fibonacci(n)
+count2 = n-2
+print(count1, count2)
