5203db8240cd4e5dd8499ef1e6d442f551554d92
KangHaSeong <shaawwert6044@gmail.com>
2023-12-25 17:56:44 +0900

    설탕배달

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,9 +1,18 @@
-chess = [input() for _ in range(8)]
-cnt = 0
+# 반복문으로 가장 긴 면을 찾기
+# 다만, 가로길이와 세로길이가 다를 경우, 둘을 바꿔서 비교하기
+def solution(size):
+    row = []
+    col = []
+    max_w = 0
+    max_h = 0
 
-for y in range(8):
-    for x in range(8):
-        if y%2 == x%2 and chess[y][x] == 'F':
-            cnt += 1
+    for i in range(len(size)):
+        if size[i][0] < size[i][1]:
+            size[i][0], size[i][1] = size[i][1], size[i][0]
+        row.append(size[i][0])
+        col.append(size[i][1])
 
-print(cnt)
+    max_w = max(row)
+    max_h = max(col)
+
+    return max_w * max_h
