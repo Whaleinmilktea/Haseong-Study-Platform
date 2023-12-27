83bc8434d7ca4f4d986215ed8252a040460781f4
KangHaSeong <shaawwert6044@gmail.com>
2023-12-27 22:30:10 +0900

    파일정리

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,3 +1,8 @@
-s = "abcd"
-print(s[-1])
-print(s[0:-1])
+# 치킨 10개 -> 쿠폰 1개 : coupon = chicken // 10
+# 치킨 10개 -> 쿠폰 1개 :
+
+
+def solution(chicken):
+    coupon = chicken // 10
+    while coupon >= 10:
+        coupon = (coupon // 10)
