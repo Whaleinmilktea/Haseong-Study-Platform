90fd06a1c1948be38df90d87533959588e613193
KangHaSeong <shaawwert6044@gmail.com>
2023-12-28 21:57:55 +0900

    행렬의 덧셈 - 프로그래미서ㅡㅡtest

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,8 +1,12 @@
-# 치킨 10개 -> 쿠폰 1개 : coupon = chicken // 10
-# 치킨 10개 -> 쿠폰 1개 :
+def solution(arr1, arr2):
+  result = []
+  for i in range(len(arr1)):
+    temp = []
+    for j in range(len(arr1[i])):
+      temp.append(arr1[i][j] + arr2[i][j])
+    result.append(temp)
+    temp = []
+  return result
 
 
-def solution(chicken):
-    coupon = chicken // 10
-    while coupon >= 10:
-        coupon = (coupon // 10)
+print(solution([[1, 2], [2, 3]], [[3, 4], [5, 6]]))
